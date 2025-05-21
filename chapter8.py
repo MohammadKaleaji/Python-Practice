30230fa634e1fd31cb2552b2cee8b34b5e0ca32b
MohammadKaleaji <mohammadkaleaji@gmail.com>
2025-05-22 01:52:53 +0300

    commit def practice

--- chapter8.py
+++ chapter8.py
@@ -23,3 +23,27 @@ def get_formatted_name(first_name, last_name):
 
 my_name = get_formatted_name('hanaa', 'benni')
 print(my_name)
+
+# Function Returns Full name, leaving the middle name optional
+
+def get_full_name(first_name, last_name, middle_name = ''): 
+    """Printing Full name"""
+    if middle_name:
+        full_name = f"{first_name} {middle_name} {last_name}"
+    else:
+       full_name = f"{first_name} {last_name}" 
+    return full_name
+
+my_full_name = get_full_name('mohammad', 'kaleaji', 'ziad')
+print(f"\n{my_full_name}")    
+
+my_full_name1 = get_full_name('mohammad', 'kaleaji')
+print(f"\n{my_full_name1}")  
+
+def car_dic(name, model):
+    """Function returns a car's name and model as a dictionary"""
+    car = {'car_name': name, 'car_mode': model}
+    return car
+
+my_car = car_dic('Honda', '2024')
+print(f'\n{my_car}\n')
