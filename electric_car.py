3fe71fd66fbc8fdb2a1ce0b8ddb51eead432dc31
MohammadKaleaji <mohammadkaleaji@gmail.com>
2025-05-26 20:58:51 +0300

    electric car oop

--- electric_car.py
+++ electric_car.py
@@ -22,10 +22,31 @@ class Car:
     def increment_odometer(self, milege):
         self.odometer += milege
 
+
+class Battery:
+    """A simple attempt to model a battery of an electric car"""
+    
+    def __init__(self, battery_size = 19):
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        print(f"The battery size of the car {self.battery_size}")   
+
+    def get_range(self):
+        if self.battery_size >= 20:
+            range = 'High'
+            print(f"The range of the car {range}")
+        else:
+            range = 'Low'  
+            print(f"The range of the car {range}")
+
 class Electric_Car(Car):
     """Inheritance from Car Class"""
     def __init__(self, make, model, year):
         super().__init__(make, model, year)
+        self.battery = Battery()   # adding new attribute to the inherited class
      
 my_tesla = Electric_Car('Tesla', 'Electric', 2025)
 print(my_tesla.car_descriptive())
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
